0e9194915ffe4ba21a0941a1f0c3d8a1c9545f69
alexpcapp <alexander.cappelen@hotmail.com>
2023-07-27 22:42:32 +0200

    Longer succes-message

--- champagneklubben/review/champagne-form.js
+++ champagneklubben/review/champagne-form.js
@@ -77,7 +77,7 @@ champagneForm.addEventListener("submit", async (e) => {
     showSuccessMessage("Review submitted successfully!");
     setTimeout(() => {
       successMessage.style.display = "none";
-    }, 3000); // Hide after 3 seconds (adjust as needed)
+    }, 8000); // Hide after 3 seconds (adjust as needed)
   } catch (error) {
     // Show an error message if there's an issue with submitting the review
     showErrorMessage("Error submitting review. Please try again later.");
