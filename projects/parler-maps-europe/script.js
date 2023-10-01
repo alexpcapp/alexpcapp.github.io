05984ea4003633f09051eb3d56b98192cc3c1efd
alexpcapp <alexander.cappelen@hotmail.com>
2023-10-01 23:07:34 +0200

    edit link to raw

--- projects/parler-maps-europe/script.js
+++ projects/parler-maps-europe/script.js
@@ -1,7 +1,8 @@
 function displayMap(mapName) {
     // Update the src attribute of the <img> element to display the selected map
     const mapImage = document.getElementById("map-image");
-    mapImage.src = `https://raw.githubusercontent.com/alexpcapp/parler-maps-europe/blob/main/maps/${mapName}.png`;
+    mapImage.src = `https://raw.githubusercontent.com/alexpcapp/parler-maps-europe/main/maps/${mapName}.png?raw=true`;
+
 }
 
 const mapSelector = document.getElementById("map-selector");
