1dd20f0082832d1a3d6b369095092122f92b3c65
alexpcapp <alexander.cappelen@hotmail.com>
2023-10-01 23:13:21 +0200

    random initial map

--- projects/parler-maps-europe/script.js
+++ projects/parler-maps-europe/script.js
@@ -1,3 +1,15 @@
+// Function to display a random map initially
+function displayRandomMap() {
+    const mapSelector = document.getElementById("map-selector");
+    const randomIndex = Math.floor(Math.random() * mapSelector.options.length);
+    const randomMapValue = mapSelector.options[randomIndex].value;
+    displayMap(randomMapValue);
+}
+
+// Call the displayRandomMap function when the page loads
+window.addEventListener("load", displayRandomMap);
+
+
 function displayMap(mapName) {
     // Update the src attribute of the <img> element to display the selected map
     const mapImage = document.getElementById("map-image");
